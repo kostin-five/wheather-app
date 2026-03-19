47a06c61d20f88fb803a5889e773a7fb08f7efc9
Bogdan <kostin052006@gmail.com>
2026-03-19 14:44:28 +0300

    Добавлено получение координат города и текущей погоды через API

--- main.js
+++ main.js
@@ -6,34 +6,79 @@ const cardResult = document.querySelector("#weatherCard");
 const lastBtn = document.querySelector("#lastSearchBtn");
 const history = document.querySelector(".history");
 
-if (history.lastElementChild.children.length === 0) {
-  history.firstElementChild.classList.add("hidden");
-} else {
-  history.firstElementChild.classList.remove("hidden");
+function notShowHistory() {
+  if (history.lastElementChild.children.length === 0) {
+    history.firstElementChild.classList.add("hidden");
+  } else {
+    history.firstElementChild.classList.remove("hidden");
+  }
 }
 
-function showError() {
+notShowHistory();
+
+function showError(msg = "Введите название города!") {
   loading.classList.add("hidden");
   cardResult.classList.add("hidden");
   messageError.classList.remove("hidden");
-  messageError.textContent = "Введите название города!";
+  messageError.textContent = msg;
 }
 
-function showLoading(city) {
+function showLoading() {
   messageError.classList.add("hidden");
   cardResult.classList.add("hidden");
   loading.classList.remove("hidden");
-  console.log(city);
 }
 
-form.addEventListener("submit", (e) => {
+async function fetchWeather(city) {
+  let url = new URL(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(
+      city
+    )}&count=1&language=ru&format=json`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  if (!data.results || data.results == []) {
+    showError("Ошибка: город не найден");
+    return;
+  }
+  const cityData = data.results[0];
+  const latitude = cityData.latitude;
+  const longitude = cityData.longitude;
+  await getWeatherData(latitude, longitude);
+}
+
+async function getWeatherData(latitude, longitude) {
+  let url = new URL(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  const currentData = data.current;
+  const temperature = currentData.temperature_2m;
+  const humidity = currentData.relative_humidity_2m;
+  const windSpeed = currentData.wind_speed_10m;
+  const weatherCode = currentData.weather_code;
+  console.log(
+    "температура: " +
+      temperature +
+      " влажность: " +
+      humidity +
+      " ветер: " +
+      windSpeed +
+      " код: " +
+      weatherCode
+  );
+}
+
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
   let city = input.value.trim();
 
   if (city === "") {
     showError();
   } else {
-    showLoading(city);
+    showLoading();
+    await fetchWeather(city);
   }
   input.value = "";
 });
